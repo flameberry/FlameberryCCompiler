33d26e344d3378f6630d68b1eed67113e18c0c01
Flameberry <flameberrydev@gmail.com>
2024-01-12 18:48:11 +0530

    Initial Commit.

---
+++ Sandbox/gcc.s
@@ -0,0 +1,12 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 2
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	mov	w0, #2
+	ret
+	.cfi_endproc
+                                        ; -- End function
+.subsections_via_symbols
