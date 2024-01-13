4185683b56829aa37b8dd3e967c8d9c657a7ad90
Flameberry <flameberrydev@gmail.com>
2024-01-13 15:56:28 +0530

    Added preprocessing of comments and basic lexing.

--- CCompiler/src/main.rs
+++ CCompiler/src/main.rs
@@ -1,3 +1,42 @@
+use std::fs;
+
+fn run_tests(testpath: &str) {
+    let directory =
+        fs::read_dir(testpath).expect(&format!("Failed to read directory: {}", testpath));
+    for testcase in directory {
+        let testcasepath = testcase
+            .expect("Failed to get the path of the test case!")
+            .path();
+
+        let testprogramsrc = {
+            let testcasepath = &testcasepath;
+            fs::read_to_string(testcasepath).unwrap_or_else(|err| {
+                panic!(
+                    "Failed to read file: {} with error: {}",
+                    testcasepath.display(),
+                    err
+                );
+            })
+        };
+
+        // Actual compilation stages
+        let preprocessed_src = flameberrycc::preprocessor::preprocess(&testprogramsrc);
+        let tokens = flameberrycc::lexer::lex(&preprocessed_src);
+
+        // Display output for debugging
+        println!("{}:", testcasepath.display());
+        println!("Original:\n{}", testprogramsrc);
+        println!("Preprocessed:\n{}", preprocessed_src);
+
+        println!("Lexed:");
+        for tk in &tokens {
+            println!("{:?}", tk);
+        }
+        println!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let testpath = "/Users/flameberry/Developer/writing-a-c-compiler-tests/tests/chapter_1/valid";
+    run_tests(testpath);
 }
---
+++ CCompiler/src/preprocessor.rs
@@ -0,0 +1,24 @@
+pub fn preprocess(source: &str) -> String {
+    let mut out = String::new();
+    // Reserve memory to avoid allocations per line for big source files
+    out.reserve(source.bytes().len());
+
+    for line in source.split('\n') {
+        // Trim to the start so that we can check for // comments or # preprocessor directives
+        let trimmed = line.trim_start();
+
+        // Ignore comments
+        if !trimmed.starts_with("//") {
+            out.push_str(line);
+            out.push('\n');
+        }
+        // Expand the preprocessor macros
+        else if trimmed.starts_with('#') {
+            panic!("Preprocessor directives not supported yet!");
+        }
+    }
+    // Remove the unncessary '\n' at the end of the program
+    out.pop();
+    // Return the preprocessed source
+    out
+}
