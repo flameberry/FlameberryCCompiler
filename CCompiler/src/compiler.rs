1bf14046819b39d2b609057f755d19f08fd5eedf
Flameberry <flameberrydev@gmail.com>
2024-02-24 18:38:02 +0530

    Added docs to compiler.rs

--- CCompiler/src/compiler.rs
+++ CCompiler/src/compiler.rs
@@ -8,6 +8,8 @@ use std::{
     path,
 };
 
+/// Contains the specification for the compiler.
+/// The specification includes the target file to be compiled.
 pub struct CompilerSpecification<'a> {
     // target_files: Vec<String>,
     pub target_file: &'a str,
@@ -18,14 +20,49 @@ pub struct Compiler<'a> {
 }
 
 impl<'a> Compiler<'a> {
+    /// Creates a new instance of the `Compiler` struct with the given `CompilerSpecification`.
+    ///
+    /// # Arguments
+    ///
+    /// * `specification` - The specification for the compiler.
+    ///
+    /// # Returns
+    ///
+    /// Returns a new `Compiler` instance.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let specification = CompilerSpecification { target_file: "main.c" };
+    /// let compiler = Compiler::new(specification);
+    /// ```
     pub fn new(specification: CompilerSpecification<'a>) -> Self {
         Compiler { specification }
     }
 
+    /// Compiles the source file specified in the `CompilerSpecification`.
+    ///
+    /// # Returns
+    ///
+    /// Returns `true` if the compilation is successful, `false` otherwise.
+    ///
+    /// # Example
+    ///
+    /// ```no_run
+    /// let specification = CompilerSpecification { target_file: "main.c" };
+    /// let compiler = Compiler::new(specification);
+    /// let result = compiler.compile();
+    /// ```
+    ///
+    /// # Panics
+    ///
+    /// Panics if there is an error reading the source file.
     pub fn compile(&self) -> bool {
+        // Read the source file
         let src = fs::read_to_string(self.specification.target_file);
         println!("Compiling {}...", self.specification.target_file);
 
+        // Unwrap the result of reading the source file, panicking if there is an error
         let src_str = src.unwrap_or_else(|err| {
             panic!(
                 "Failed to read source file: {}: with error: {}",
@@ -33,18 +70,20 @@ impl<'a> Compiler<'a> {
             )
         });
 
+        // Create a new instance of the parser
         let mut parser = Parser::new(&src_str);
 
+        // Parse the source file
         match parser.parse() {
             Ok(translation_unit) => {
-                // println!("{:?}", translation_unit),
+                // Display the parsed translation unit
                 display_translationunit(&translation_unit);
 
                 match SemanticAnalyzer::analyze(&translation_unit) {
                     Ok(()) => {
                         println!("Semantic Analysis was successful");
 
-                        // This `if` statement is for developer debugging convenience
+                        // This `if` statement is for developer debugging convenience, to toggle the assembly generation
                         if true {
                             // Create an instance of AssemblyGenerator
                             let mut assembly_generator = AssemblyGenerator::new();
@@ -66,6 +105,7 @@ impl<'a> Compiler<'a> {
                                     assemblyfile.write_all(assembly.as_bytes()).unwrap();
                                     true
                                 }
+                                // If there is an error in the assembly generation, print the error and return false
                                 Err(err) => {
                                     println!("{}:{}", self.specification.target_file, err);
                                     false
@@ -75,12 +115,14 @@ impl<'a> Compiler<'a> {
                             true
                         }
                     }
+                    // If there is an error in the semantic analysis, print the error and return false
                     Err(err) => {
                         println!("{}:{}", self.specification.target_file, err);
                         false
                     }
                 }
             }
+            // If there is an error in the parsing, print the error and return false
             Err(err) => {
                 println!("{}:{}", self.specification.target_file, err);
                 false
